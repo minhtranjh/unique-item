68843c74cb330fb30761b9c13ef7a72d025a17f5
minhtranjh <83933288+minhtranjh@users.noreply.github.com>
2021-06-01 00:04:45 +0700

    Change naming

--- repeatition.js
+++ repeatition.js
@@ -6,16 +6,16 @@ const array = [1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 4, 4, 4, 5, 5];
 //Maximum 3 rounds, not nested.
 
 const removeDuplicates = (arr) => {
-  let flag = {};
-  let seen = {};
+  let flagRepetition = {};
+  let flagSeen = {};
 
   
   //1 Round
   const newArr = arr.reduce((tempArr, item, index) => {
-    if (flag[item] === undefined || flag[item] > 0) {
-      flag[item] = flag[item] == undefined ? 1 : flag[item] + 1;
-      if (seen[item] !== 1) {
-        seen[item] = 1;
+    if (flagRepetition[item] === undefined || flagRepetition[item] > 0) {
+      flagRepetition[item] = flagRepetition[item] == undefined ? 1 : flagRepetition[item] + 1;
+      if (flagSeen[item] !== 1) {
+        flagSeen[item] = 1;
         return [
           ...tempArr,
           {
@@ -33,7 +33,7 @@ const removeDuplicates = (arr) => {
   let maxArr = [];
   let maxValue = newArr[0].repeatitions;
   newArr.forEach((item, index) => {
-    newArr[index].repeatitions = flag[item.value];
+    newArr[index].repeatitions = flagRepetition[item.value];
     if (newArr[index].repeatitions >= maxValue) {
       if (newArr[index].repeatitions > maxValue) {
         maxValue = newArr[index].repeatitions;
@@ -42,7 +42,10 @@ const removeDuplicates = (arr) => {
       maxArr.push(newArr[index]);
     }
   });
-  return maxArr;
+  return {
+    repetitions : newArr,
+    maxRepetitions : maxArr,
+  };
 };
 
 removeDuplicates(array);
