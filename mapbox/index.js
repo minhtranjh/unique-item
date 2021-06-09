ca04ddf6e72e6cdf28f9646390768e0438b03906
minhtranjh <minh.tran@journeyh.io>
2021-06-09 11:44:46 +0700

    Fix map box search feature

--- mapbox/index.js
+++ mapbox/index.js
@@ -3,7 +3,6 @@ const loading = document.querySelector(".fa-spinner");
 const suggestions = document.querySelector(".suggestions");
 const input = document.getElementById("search-input");
 const backToBtn = document.querySelector(".back-to-btn");
-let typingTimeout;
 let currentLocation;
 mapboxgl.accessToken = `pk.eyJ1IjoibWludHRyYW45MDAxIiwiYSI6ImNrcGhwM2R2ZjA1ajQycG43dDc1Nmo5dGwifQ.xFeLkx7CYf2NbuU01uXt0g`;
 navigator.geolocation.getCurrentPosition(successCb, errorCb, {
@@ -93,37 +92,45 @@ function moveToPlace(lng, lat, type) {
     zoom,
   });
   currentLocation = [lng, lat, type];
-  suggestions.style.display = "none"
+  setTimeout(() => {
+    suggestions.classList.remove("view");
+  });
 }
 
-let isCounting = false;
+let isFetching = false;
+let typingTimeout;
 let queryTimeout;
 function delay(query) {
   if (typingTimeout) {
     clearTimeout(typingTimeout);
   }
   typingTimeout = setTimeout(() => {
-    suggestions.style.display = query ? "block" : "none";
+    query
+      ? suggestions.classList.add("view")
+      : suggestions.classList.remove("view");
     query = query.replace(/\//g, "%2F");
+    clearTimeoutFixedFetch();
     query && fetchData(query.toLowerCase());
   }, 500);
 }
- function delayFixedFetch(query) {
-  isCounting = true;
+function delayFixedFetch(query) {
+  isFetching = true;
   queryTimeout = setTimeout(() => {
-    console.log(query);
-    suggestions.style.display = "block"
+    suggestions.classList.add("view");
     query = query.replace(/\//g, "%2F");
-    query
-      ? fetchData(query.toLowerCase(), "delayed")
-      : clearTimeoutFixedFetch("delayed");
-  }, 1500);
+    query ? fetchData(query.toLowerCase()) : clearTimeoutFixedFetch();
+  }, 1200);
+}
+function clearTimeoutFixedFetch() {
+  if (isFetching) {
+    isFetching = false;
+    if (queryTimeout) clearTimeout(queryTimeout);
+  }
 }
 input.addEventListener("keyup", (e) => {
-  if (e.which <= 90 && e.which >= 48 || e.which===8)
-  {
+  if ((e.which <= 90 && e.which >= 48) || e.which === 8) {
     const query = e.target.value;
-    !isCounting && delayFixedFetch(query);
+    !isFetching && delayFixedFetch(query);
     delay(query);
     query
       ? loading.classList.add("loading")
@@ -133,13 +140,14 @@ input.addEventListener("keyup", (e) => {
 
 //Stop bubbling
 document.body.addEventListener("click", () => {
-  suggestions.style.display = "none";
+  suggestions.classList.remove("view");
 });
 document.querySelector(".search-box").addEventListener("click", (e) => {
-  suggestions.style.display = "block";
+  suggestions.classList.add("view");
   e.stopPropagation();
 });
 //Stop bubbling
+
 async function fetchByCoordinates(lng, lat) {
   try {
     const promise = await fetch(
@@ -150,60 +158,48 @@ async function fetchByCoordinates(lng, lat) {
     console.log(error);
   }
 }
-function clearTimeoutFixedFetch(type) {
-  if (type && isCounting) {
-    isCounting = false;
-    if (queryTimeout) clearTimeout(queryTimeout);
-  }
-}
+
 async function fetchData(query, type) {
   try {
     const promise = await fetch(
       `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${mapboxgl.accessToken}`
     );
     if (!promise?.ok) {
-      clearTimeoutFixedFetch(type)
       return;
     }
-    const data = await promise.json();
-
     loading.classList.remove("loading");
-
-    if (!data?.features) {
-      return;
-    }
-    if (data.features.length === 0) {
-      clearTimeoutFixedFetch(type)
+    const data = await promise.json();
+    if (!data?.features || data.features.length === 0) {
       suggestions.innerHTML = `
       <div class="sug">
           <span>No place found</span>
           </div>
       `;
-      return;
-    }
-
-    suggestions.innerHTML = "";
-
-    data.features.forEach((item, index) => {
-      clearTimeoutFixedFetch(type)
-      suggestions.innerHTML += `
-      <div onclick="moveToPlace(${
-        item.center[0] + "," + item.center[1] + "," + `'${item.place_type[0]}'`
-      })" class="sug">
-      <i class="fas fa-map-marker-alt"></i>
-          <span>${item.place_name}</span>
+    } else {
+      suggestions.innerHTML = "";
+      data.features.forEach((item, index) => {
+        suggestions.innerHTML += `
+        <div onclick="moveToPlace(${
+          item.center[0] +
+          "," +
+          item.center[1] +
+          "," +
+          `'${item.place_type[0]}'`
+        })" class="sug">
+            <i class="fas fa-map-marker-alt"></i>
+            <span>${item.place_name}</span>
           </div>
-        
-        `;
-    });
+          
+          `;
+      });
+    }
   } catch (error) {
     loading.classList.add("remove");
-    clearTimeoutFixedFetch(type)
     suggestions.innerHTML = `
     <div class="sug">
       <i class="fas fa-map-marker-alt"></i>
-          <span>${error.message}</span>
-          </div>
+      <span>Something went wrong</span>
+    </div>
       `;
   }
 }
