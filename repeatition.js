151dd3a0f4b601c9536fa38fdd2baa4491c4fa69
minhtranjh <minh.tran@journeyh.io>
2021-06-11 10:56:40 +0700

    Optimize repeatition.js

--- repeatition.js
+++ repeatition.js
@@ -1,4 +1,4 @@
-const array = [1, 1,1, 1, 2, 2, 2, 3, 3, 5, 5, 5];
+const array = [1, 2, 3, 3, 4, 4, 5];
 //Ex 2: Given an array of integers, find the most
 //repetitions and repetitions.
 //If multiple numbers have the same maximum number of
@@ -9,33 +9,23 @@ const findMaxRepeatitionsArray = (arr) => {
   let flagRepetition = {};
   let flagIndex = {};
   let index = 0;
-  let newArr = [];
   let maxValue = 0;
   const maxArr = [];
-  for (let i = 0; i < arr.length; i++) {
-      flagRepetition[arr[i]] =
-        flagRepetition[arr[i]] === undefined ? 1 : flagRepetition[arr[i]] + 1;
-      if (flagRepetition[arr[i]] >= 1) {
-        if (flagRepetition[arr[i]] === 1) {
-          newArr.push({
-            value: arr[i],
-            repetitions: 1,
-          });
-          flagIndex[arr[i]] = index++;
-        }
-        newArr[flagIndex[arr[i]]].repetitions = flagRepetition[arr[i]];
-        if (maxValue <= newArr[flagIndex[arr[i]]].repetitions) {
-          if (maxValue === newArr[flagIndex[arr[i]]].repetitions) {
-            maxValue = newArr[flagIndex[arr[i]]].repetitions;
-            maxArr.push(newArr[flagIndex[arr[i]]]);
-          } else {
-            maxArr.length = 0;
-            maxValue = newArr[flagIndex[arr[i]]].repetitions;
-            maxArr.push(newArr[flagIndex[arr[i]]]);
-          }
-        }
-      }
+  arr.reduce((newArr, item) => {
+    flagRepetition[item] =
+      flagRepetition[item] === undefined ? 1 : flagRepetition[item] + 1;
+    if (flagRepetition[item] === 1) {
+      newArr.push({ value: item, repetitions: 1 });
+      flagIndex[item] = index++;
     }
+    newArr[flagIndex[item]].repetitions = flagRepetition[item];
+    if (maxValue <= newArr[flagIndex[item]].repetitions) {
+      if (maxValue !== newArr[flagIndex[item]].repetitions) maxArr.length = 0;
+      maxValue = newArr[flagIndex[item]].repetitions;
+      maxArr.push(newArr[flagIndex[item]]);
+    }
+    return newArr;
+  }, []);
   return maxArr;
 };
 console.log(findMaxRepeatitionsArray(array));
