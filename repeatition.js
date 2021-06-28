19549679a112dde9a8c9ab027f8f486b255263b5
tam.vu <vuthanhtam1506@gmail.com>
2021-06-28 10:33:03 +0700

    Refactor repetition function to make it clearer

--- repeatition.js
+++ repeatition.js
@@ -1,23 +1,30 @@
-const array = [1, 2, 3, 3, 3, 3, 3, 4,4,4,4, 4, 5];
 //Ex 2: Given an array of integers, find the most
 //repetitions and repetitions.
 //If multiple numbers have the same maximum number of
 //iterations, export all of them.
 //Maximum 3 rounds, not nested.
-const findMaxRepeatitionsArray = (arr) => {
+const findMaxRepetitionsArray = (arr) => {
   const result = arr.reduce(
-    (acc, item) => {
-      acc.repeatList[item] =
-        acc.repeatList[item] === undefined ? 1 : acc.repeatList[item] + 1;
-      acc.repeatList[item] = acc.repeatList[item];
-      if (acc.max <= acc.repeatList[item]) {
-        if (acc.max !== acc.repeatList[item]) acc.maxList.length = 0;
-        acc.max = acc.repeatList[item];
-        acc.maxList.push({ value: item, repeatition: acc.repeatList[item] });
+    (currentResult, currentItem) => {
+      const currentItemRepeatTime =
+        currentResult.mapToCountRepeatTime[currentItem] === undefined
+          ? 1
+          : currentResult.mapToCountRepeatTime[currentItem] + 1;
+
+      currentResult.mapToCountRepeatTime[currentItem] = currentItemRepeatTime;
+
+      if (currentResult.maxRepeat <= currentItemRepeatTime) {
+        if (currentResult.maxRepeat !== currentItemRepeatTime) {
+          currentResult.maxList.length = 0;
+        }
+
+        currentResult.maxRepeat = currentItemRepeatTime;
+        currentResult.maxList.push(currentItem);
       }
-      return acc;
+
+      return currentResult;
     },
-    { max: 0, maxList: [], repeatList: {} }
+    { maxRepeat: 0, maxList: [], mapToCountRepeatTime: {} }
   );
   return result.maxList;
 };
